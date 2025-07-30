178998b417eb1104d3fab2235bbaa183ed4550ef
ImmanuelAj <152216534+ImmanuelAj@users.noreply.github.com>
2025-07-30 15:11:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 15; ++i){
+  // console.log(i)
+
+  if(i % 5 === 0){
+    console.log(`${i} Asap Frontend`)
+  }
+
+  else if(i % 2 === 0){
+    //when consoling, we use dollar signs and bracket to add variables to string!!
+    console.log(`${i} - Frontend`)
+  }
+
+  else{
+    console.log(`${i} - Asap`)
+  }
+}
